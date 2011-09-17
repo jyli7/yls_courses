23905f4cfdb969747e3ecb4ede46281cce831b23
jimmyli <jyl702@gmail.com>
2011-09-16 22:36:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YlsCourses::Application.config.secret_token = 'be966e8ed9756945b92ff2e421400d96dd2d35a0a6de35949f3e25e7663d9827a87359c07be189d85af23030c87572bed2836e7313dfe680dc7a662105e421ef'
