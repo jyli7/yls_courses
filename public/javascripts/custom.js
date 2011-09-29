d45ae25a45e6b831074046a8b53c9837a8541188
jimmyli <jyl702@gmail.com>
2011-09-29 14:58:31 -0400

    Javascript added

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,20 +1,16 @@
 $(document).ready(function() {
 	
-  $('#course_list tr:even').addClass('zebra'); /*horizontal bars in the courses table */
-  $('tr.course_descrip').hide();
+  $('.other_rows:even').addClass('zebra'); /*horizontal bars in the courses table */
+  $('.descrip_body').hide();
   $('.course_name_click').click(function() {
-	$(this).closest('tr').next().slideToggle('slow');
+	$(this).closest('div').next().slideToggle('slow');
 	return false;
   });
 
- 
 });
 
 
 $('td.course_number').click(function() {
 	$(this).next().hide();  
 });
-
-
-
 
