ac94b472ac8d56034868ba748120e67e3940d6b6
jimmyli <jyl702@gmail.com>
2011-10-01 17:43:48 -0400

    Deleted migration files

--- db/migrate/20110922161811_remove_end_time_from_course.rb
+++
@@ -1,9 +0,0 @@
-class RemoveEndTimeFromCourse < ActiveRecord::Migration
-  def self.up
-    remove_column :courses, :end_time
-  end
-
-  def self.down
-    add_column :courses, :end_time, :datetime
-  end
-end
--- db/migrate/20110922161940_remove_start_time_from_course.rb
+++
@@ -1,9 +0,0 @@
-class RemoveStartTimeFromCourse < ActiveRecord::Migration
-  def self.up
-    remove_column :courses, :start_time
-  end
-
-  def self.down
-    add_column :courses, :start_time, :datetime
-  end
-end
