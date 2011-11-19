bb5427887e63ec9b5e0ba1fe9ff1ae7d4fd103be
jimmyli <jyl702@gmail.com>
2011-11-19 13:38:45 -0500

    Added host default url option to production

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 YlsCourses::Application.initialize!
 
+config.action_mailer.default_url_options = { :host => 'localhost:3000' }  
+
 # ActionMailer::Base.delivery_method = :smtp
 # ActionMailer::Base.smtp_settings = {
 #    :tls => true,
