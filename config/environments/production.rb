7fe07a709a1afdc0e50a5b647003404e97644e3a
jimmyli <jyl702@gmail.com>
2011-11-19 13:43:53 -0500

    Commeted out host in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ YlsCourses::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'www.ylsclassaction.com' }  
+  # config.action_mailer.default_url_options = { :host => 'www.ylsclassaction.com' }  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
